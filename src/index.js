205ad4a50ef6fa4b4bb3c96c7552b040af4c4e5f
RhexNiebres <rhexniebres@gmail.com>
2025-01-08 15:39:52 -0800

    add append function that adds a new node to the end of the list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+class Node{
+  constructor(value){
+    this.value = null;
+    this.nextNode = null;
+  }
+}
+
 class LinkedList{
   constructor(){
     this.head = null;//the head starts with null or as empty
   }
-}
 
-class Node{
-  constructor(){
-    this.value = null;
-    this.nextNode = null;
+  append(value){
+    const newNode = new Node(value)//new node for constructor
+    
+    if(this.head === null){
+      this.head = newNode;//if list is empty set this as the head
+    }else{
+      let currentNode = this.head;
+      while(currentNode.nextNode !== null){
+        currentNode = currentNode.nextNode;
+      }
+    }
+  }
+
+  prepend(value) {
+  const newNode = new Node(value);
+
+  if(this.head === null){
+    this.head = newNode;
+  }
   }
 }
