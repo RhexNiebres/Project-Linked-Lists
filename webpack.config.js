a6d7ac16f1ec2f15e3c22115be3f80c2652fd934
RhexNiebres <rhexniebres@gmail.com>
2025-01-08 14:30:17 -0800

    add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js', // Entry point for JavaScript
+  entry: './src/index.js',  
   output: {
-    path: path.resolve(__dirname, 'dist'), 
-    filename: 'bundle.js', 
+    path: path.resolve(__dirname, 'dist'),  
+    filename: 'bundle.js',  
   },
-  mode: 'development', 
+  mode: 'development',  
   devServer: {
     static: './dist', 
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './dist/index.html',
+      template: './src/index.html',  
     }),
   ],
 };
