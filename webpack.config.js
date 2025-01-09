d0eb12b2a36b5fe567ed740b5acf781cfa6b614b
RhexNiebres <rhexniebres@gmail.com>
2025-01-08 17:41:50 -0800

    refactor webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,21 @@ module.exports = {
   devServer: {
     static: './dist', 
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Process .js files
+        exclude: /node_modules/, // Exclude node_modules
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'], // Transpile modern JavaScript to older versions
+            sourceType: 'module', // Ensure the JavaScript is treated as a module
+          },
+        },
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',  
